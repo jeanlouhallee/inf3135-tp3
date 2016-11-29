6eec86bc38b39a40319169ef22c5bf41b7ee0fef
Alexandre Blondin Massé <alexandre.blondin.masse@gmail.com>
2016-11-28 23:25:48 -0500

    Testing room_partition.

--- src/tp3.c
+++ src/tp3.c
@@ -3,39 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include "parse_args.h"
-#include "array.h"
 #include "maze.h"
-#include "room_partition.h"
 #include "drawing.h"
-
-void testMaze() {
-    struct Maze *maze = Maze_randomMaze(8, 10);
-    Maze_print(maze, false);
-    printf("Are rooms consistent? %s\n", Maze_areRoomsConsistent(maze) ? "yes" : "no");
-    Maze_free(maze);
-}
-
-void testPartition() {
-    struct RoomPartition partition = RoomPartition_create(4, 3);
-    RoomPartition_print(&partition);
-    RoomPartition_merge(&partition, 0, 0, 0, 1);
-    RoomPartition_print(&partition);
-    RoomPartition_merge(&partition, 0, 0, 0, 2);
-    RoomPartition_print(&partition);
-    RoomPartition_merge(&partition, 0, 0, 0, 1);
-    RoomPartition_print(&partition);
-}
-
-void testArray() {
-    struct Array *array = Array_create();
-    struct uiPair pair;
-    pair.first = 2; pair.second = 3;
-    Array_append(array, &pair);
-    pair.first = 5; pair.second = 7;
-    Array_append(array, &pair);
-    Array_print(array);
-    Array_delete(array);
-}
 
 int main(int argc, char **argv) {
     struct Arguments arguments = parseArguments(argc, argv);
